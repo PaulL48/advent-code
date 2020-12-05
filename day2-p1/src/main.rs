7061b0be16e6ee391fb5bd5292d5c1030c6827c5
Paul Lane <paul.lane083@gmail.com>
2020-12-04 22:22:08 -0500

    Running rustfmt

--- day2-p1/src/main.rs
+++ day2-p1/src/main.rs
@@ -1,6 +1,6 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
-use std::collections::HashMap;
 
 fn character_counts(string: &str) -> HashMap<char, i32> {
     let mut character_counts = HashMap::new();
@@ -12,18 +12,34 @@ fn character_counts(string: &str) -> HashMap<char, i32> {
 }
 
 fn is_password_valid(line: &str) -> bool {
-    let tokens: Vec<&str> = line.split(&['-', ' ', ':'][..]).filter(|token| !token.is_empty()).collect();
-    let (min, max, character, password) = (tokens[0].parse::<i32>().unwrap(), tokens[1].parse::<i32>().unwrap(), &tokens[2].chars().next().unwrap(), tokens[3]);
+    let tokens: Vec<&str> = line
+        .split(&['-', ' ', ':'][..])
+        .filter(|token| !token.is_empty())
+        .collect();
+    let (min, max, character, password) = (
+        tokens[0].parse::<i32>().unwrap(),
+        tokens[1].parse::<i32>().unwrap(),
+        &tokens[2].chars().next().unwrap(),
+        tokens[3],
+    );
     let character_counts = character_counts(password);
-    println!("min {}, max {}, char {}, pwd {}, valid: {}", min, max, character, password, *character_counts.get(&character).unwrap_or(&0) >= min && *character_counts.get(&character).unwrap_or(&0) <= max);
-    return min <= *character_counts.get(&character).unwrap_or(&0) && *character_counts.get(&character).unwrap_or(&0) <= max;
+    println!(
+        "min {}, max {}, char {}, pwd {}, valid: {}",
+        min,
+        max,
+        character,
+        password,
+        *character_counts.get(&character).unwrap_or(&0) >= min
+            && *character_counts.get(&character).unwrap_or(&0) <= max
+    );
+    return min <= *character_counts.get(&character).unwrap_or(&0)
+        && *character_counts.get(&character).unwrap_or(&0) <= max;
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
-    let contents = fs::read_to_string(filename)
-        .expect("You gotta supply a file bro");
+    let contents = fs::read_to_string(filename).expect("You gotta supply a file bro");
 
     let mut bad_passwords = 0;
     for line in contents.lines() {
